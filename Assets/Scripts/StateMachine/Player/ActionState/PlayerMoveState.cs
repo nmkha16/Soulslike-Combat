d467a0f0225fd0bc2c9d46b8ce8076ef431cd82c
Nguyễn Minh Kha <nmkha19@clc.fitus.edu.vn>
2023-09-01 09:19:59 +0700

    4 directional movement souls like

--- Assets/Scripts/StateMachine/Player/ActionState/PlayerMoveState.cs
+++ Assets/Scripts/StateMachine/Player/ActionState/PlayerMoveState.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerMoveState : PlayerBaseState
 {
-    private readonly int moveSpeedHash = Animator.StringToHash("MoveSpeed");
+    //private readonly int moveSpeedHash = Animator.StringToHash("MoveSpeed");
+    private readonly int moveXHash = Animator.StringToHash("MoveX");
+    private readonly int moveYHash = Animator.StringToHash("MoveY");
     private readonly int moveBlendTreeHash = Animator.StringToHash("MoveBlendTree");
 
     private const float animationDampTime = 0.1f;
@@ -26,9 +28,12 @@ public class PlayerMoveState : PlayerBaseState
             playerStateMachine.SwitchState(new PlayerFallState(playerStateMachine));
         }
         CalculateMoveDirection();
-        FaceMoveDirection();
+        //FaceMoveDirection();
         Move();
-        playerStateMachine.animator.SetFloat(moveSpeedHash, playerStateMachine.inputReader.moveComposite.sqrMagnitude > 0f ? 1f : 0f, animationDampTime,Time.deltaTime);
+        //playerStateMachine.animator.SetFloat(moveSpeedHash, playerStateMachine.inputReader.moveComposite.sqrMagnitude > 0f ? 1f : 0f, animationDampTime,Time.deltaTime);
+        
+        playerStateMachine.animator.SetFloat(moveXHash,playerStateMachine.inputReader.moveComposite.x, animationDampTime, Time.deltaTime);
+        playerStateMachine.animator.SetFloat(moveYHash,playerStateMachine.inputReader.moveComposite.y, animationDampTime, Time.deltaTime);
     }
 
     public override void Exit()
